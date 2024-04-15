adf808132908cc88b8bc28e9a25700925cf72cdb
Robert W <rwilary@wisc.edu>
2024-04-14 21:59:14 -0500

    removing old unneeded functions

--- public/app.js
+++ public/app.js
@@ -577,38 +577,6 @@ function removeContent() {
   r_e(`#${id}`).innerHTML = "";
 }
 
-function payment_color() {
-  if (document.getElementById("payment_status").innerHTML == "Paid") {
-    document.getElementById("payment_status").style.color = "Green";
-  } else if (document.getElementById("payment_status").innerHTML == "Unpaid") {
-    document.getElementById("payment_status").style.color = "Red";
-  }
-}
-
-function trip_color() {
-  if (document.getElementById("trip_status").innerHTML == "Pending") {
-    document.getElementById("trip_status").style.color = "Yellow";
-  } else if (document.getElementById("trip_status").innerHTML == "Accepted") {
-    document.getElementById("trip_status").style.color = "Green";
-  }
-}
-
-function capacity_color() {
-  if (
-    document.getElementById("capacity_status").innerHTML > 11 / 12 &&
-    document.getElementById("capacity_status").innerHTML >= 7 / 12
-  ) {
-    document.getElementById("capacity_status").style.color = "Yellow";
-  } else if (
-    document.getElementById("capacity_status").innerHTML < 6 / 12 &&
-    document.getElementById("capacity_status").innerHTML > 0
-  ) {
-    document.getElementById("capacity_status").style.color = "Green";
-  } else if (document.getElementById("capacity_status").innerHTML == 12 / 12) {
-    document.getElementById("capacity_status").style.color = "Red";
-  }
-}
-
 // Get Upcoming Trips
 
 function moreDetails(tripid) {
