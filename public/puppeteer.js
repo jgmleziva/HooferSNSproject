acb8b97f4c2e5b53b0b89bb6fb0b7d13754a4c20
Ryan <Ryan@email.com>
2024-04-25 15:51:06 -0500

    Added comments on puppeteer testing

--- public/puppeteer.js
+++ public/puppeteer.js
@@ -1,1 +1,19 @@
 // Puppeteer
+
+// User Create Account
+
+// User Login
+
+// User Trip Sign-Up
+
+// User Sign-Out
+
+// Admin Login
+
+// Admin Create Trip
+
+// Admin Delete Trip
+
+// Admin Sign-Out
+
+
