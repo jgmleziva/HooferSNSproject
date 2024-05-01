607f3b9c3f9e94b0d67cd7f94d880c44d2ee9717
Ryan <Ryan@email.com>
2024-05-01 16:52:45 -0500

    updated tester

--- public/puppeteer.js
+++ public/puppeteer.js
@@ -13,18 +13,25 @@ async function go() {
 
   // User Login
   await page.click("#signinbutton");
-  await page.type("#email2", "test929@gmail.com");
+  await page.type("#email2", "testryan5@gmail.com");
   await page.type("#password2", "password");
   await page.click("#signin_form > div.pt-4 > button.button.is-primary");
 
+  // Update User Info
+  await page.click("#accountinfo");
+  await page.click("#\\37 OvsFHejaoiF1MnVawuE > button.is-pulled-right.button.is-primary.is-small.is-rounded");
+  await page.click("");
+
+  /*
   // User Trip Sign-Up - will need to be updated
-  await page.click("#\\31 714425522289");
+  await page.click("#\\31 714425522289"); //Selector here never registers and does not work
   await page.click("#su714425522289");
   await page.click(
     "#form714425522289 > div:nth-child(5) > p > input.radio.has-text-white.mr-1"
   );
   await page.click("#submit714425522289");
   await page.click("#close714425522289");
+  */
 
   // User Sign-Out
   await page.click("#navbar > nav > ul > div:nth-child(2) > div > a");
@@ -39,7 +46,7 @@ async function go() {
 
   // Admin Create Trip
   await page.click("#addeventbtn");
-  await page.type("#eventName", "Puppeteer Event");
+  await page.type("#eventName", "Puppeteer Event 2");
   await page.type("#trip_price", "$123");
   await page.type("#trip_location", "Puppet Town");
   await page.type("#trip_date", "04-25-2024");
